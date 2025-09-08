07632d40904d5773623d4a36c10018975c84a362
Caio <cfxd99@gmail.com>
2025-09-08 15:53:56 -0300

    added migrations and models based on database model file in docs folder

---
+++ backend/database/migrations/2025_09_08_183549_create_urls_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('urls', function (Blueprint $table) {
+            $table->id();
+            $table->string('original');
+            $table->string('shortened', 8)->unique();
+            $table->timestamp('expires_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('urls');
+    }
+};
