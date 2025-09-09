a7da7b937e9f219482c994bce42c0ade2530e2f3
Caio <cfxd99@gmail.com>
2025-09-08 22:38:06 -0300

    added schedule command to disable expired urls

---
+++ backend/app/Console/Commands/DisableExpiredUrlsCommand.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Url;
+use Illuminate\Console\Command;
+
+class DisableExpiredUrlsCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:disable-expired-urls-command';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        logger(now()->toDateTimeString());
+        $expiredUrls = Url::where('expires_at', '<=', now()->toDateTimeString())
+            ->where('active', 1)
+            ->get();
+
+        foreach ($expiredUrls as $expiredUrl) {
+            $expiredUrl->active = 0;
+            $expiredUrl->save();
+        }
+
+        return 'a';
+    }
+}
